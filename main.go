b151c6630d066146819b56d07cd0236a875addad
hpark-202 <devops.park@gmail.com>
2023-02-01 15:38:01 -0800

    init helloHandler function

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./root"))
 	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("String server at 8080\n")
+
 }
